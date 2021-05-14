d567b01d5ccb70e395da35a5cc528b772764dab4
Bright Lin <brightlin777@gmail.com>
2021-05-14 19:41:32 -0400

    https test

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+const fs = require("fs");
+
+const https = require("https");
+
+const server = https.createServer(
+  {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert"),
+  },
+  app
+);
+
 const express_api = require("./api/express-api");
 
 app.set("view engine", "ejs");
@@ -12,4 +24,4 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.use(express_api);
 
-app.listen(process.env.PORT || 80);
+server.listen(80 || process.env.PORT);
