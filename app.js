2c2e3e4b45845e94dd3ece4b75ea43640e6f9a90
Bright Lin <brightlin777@gmail.com>
2021-05-14 19:58:53 -0400

    revert

--- app.js
+++ app.js
@@ -2,18 +2,6 @@ const express = require("express");
 
 const app = express();
 
-const fs = require("fs");
-
-const https = require("https");
-
-const server = https.createServer(
-  {
-    key: fs.readFileSync("server.key"),
-    cert: fs.readFileSync("server.cert"),
-  },
-  app
-);
-
 const express_api = require("./api/express-api");
 
 app.set("view engine", "ejs");
@@ -24,4 +12,4 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.use(express_api);
 
-server.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 80);
