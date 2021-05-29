0bd276e75b532acc1603c97ca7f81894f065b2c7
Bright Lin <brightlin777@gmail.com>
2021-05-28 22:17:08 -0400

    correct, bigger

--- public/dom/games-dom.js
+++ public/dom/games-dom.js
@@ -32,9 +32,12 @@ export const ui = function () {
   return document.getElementById("ui");
 };
 
-export const question_answer = function (question, answers) {
+export const question_answer = function (index, question, answers) {
   const div_element = document.createElement("div");
   div_element.setAttribute("class", "question-answer");
+
+  const p_index = p(index + ".");
+  div_element.appendChild(p_index);
 
   const p_element = p(question);
   div_element.appendChild(p_element);
@@ -65,6 +68,8 @@ export const quiz = function (quiz_config, handler) {
         good += 1;
       } else {
         question_answers[i].style.border = "10px solid red";
+        let correct = p("CORRECT ANSWER : " + quiz_config[i].correct);
+        question_answers[i].parentNode.appendChild(correct);
       }
     }
     handler(good);
@@ -72,6 +77,7 @@ export const quiz = function (quiz_config, handler) {
 
   for (let i = 0; i < quiz_config.length; i++) {
     const question_answer_element = question_answer(
+      i + 1,
       quiz_config[i].question,
       quiz_config[i].answers
     );
@@ -82,6 +88,10 @@ export const quiz = function (quiz_config, handler) {
   const button_element = button("VALIDER", function () {
     check_complete();
   });
+  button_element.style.padding = "20px";
+  button_element.style.border = "2px solid white";
+  button_element.style.margin = "10px";
+  button_element.style.borderRadius = "10px";
   div_element.appendChild(button_element);
 
   return div_element;
